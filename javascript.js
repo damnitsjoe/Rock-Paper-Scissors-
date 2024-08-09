31a4f56349aa24041cfda1219cdcb0992023cc04
damnitsjoe <damnitsjoe@gmail.com>
2024-08-09 09:13:20 -0600

    Buggy build still returning console.log wrong

--- javascript.js
+++ javascript.js
@@ -1,15 +1,18 @@
 
-let compChoices = ["rock", 'paper', 'scissors']
+let compChoices = ['rock', 'paper', 'scissors']
+let humanScore = 0; 
+let computerScore = 0; 
+let choice = prompt('Rock, Paper or Sicssors');
 
 //Write logic to get computer choice. 
 function getComputerChoice() {
-    //have computer generte a random number associated with
+    //have computer generte a random number associated with a choice
     return compChoices[Math.floor(compChoices.length * Math.random())];
 }
+
 //write logic for human player 
 function getHumanChoice() {
 //input for human player to choose between R, P, S. 
-    let choice = prompt(compChoices);
     switch (choice) {
         //human choices
         case 'rock':
@@ -24,10 +27,30 @@ function getHumanChoice() {
         default: 
             console.log('you left the answer blank')
         break;
+    }}
+
+//logic function to decide who wins based on choices. 
+function playRound() {
+    if (getHumanChoice == getComputerChoice) {
+        console.log('We have a tie!');
+    } else if (
+    //else if for winning/losing pairs
+        (getHumanChoice === 'rock' && getComputerChoice === 'scissors') ||
+        (getHumanChoice === 'scissors' && getComputerChoice === 'paper') ||
+        (getHumanChoice === 'paper' && getComputerChoice === 'rock')
+    ){
+        console.log('You Win!');
+    } else {
+        console.log('Computer Wins!')
     }
+    }
+
+
     
-}
+
 //figure out how to let humans input their choice and return a result 
 console.log(compChoices);
-getHumanChoice();
+getHumanChoice(choice);
+getComputerChoice();
 console.log(getComputerChoice());
+playRound(getComputerChoice, getHumanChoice);
